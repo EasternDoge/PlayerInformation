cca6669015417646a3b5925d4c3c36b6ab09b101
EasternDoge <127234686+EasternDoge@users.noreply.github.com>
2023-12-22 11:45:06 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'EasternDoge'
+description 'Open Source Information Resource'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/index.js'
+}
